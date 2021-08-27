759375caa5cc8ad645fbc4f3f1563257eb8b5fca
lingxinglovecode <80627935+lingxinglovecode@users.noreply.github.com>
2021-08-28 00:10:13 +0800

    1

--- java/src/Practice/java_class.java
+++ java/src/Practice/java_class.java
@@ -1,2 +1,69 @@
-package Practice;public class java_class {
+package Practice;
+
+import javax.swing.text.StyledEditorKit;
+
+public class java_class {
+    public static void main(String[] args){
+    Account my_account = new Account();
+    my_account.cid = "12345567";
+    my_account.balance = 10000;
+    my_account.name = "lingxing";
+    my_account.getInfo();
+    my_account.withdraw(200);
+    my_account.getInfo();
+    Customer lingxing = new Customer();
+    System.out.println(lingxing.say());
+    }
+}
+
+
+
+class Account{
+    String cid ;
+    int balance = 1234;
+    String name;
+
+    public void getInfo(){
+        System.out.println("卡号\t\t\t余额\t\t姓名");
+        System.out.println(cid+'\t'+balance+'\t'+name);
+    }
+    public boolean withdraw(int num){
+        if(num>=0 && balance>=num)
+        {
+            balance = balance - num;
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public boolean save(int num){
+        if(num<=0){
+            return false;
+        }
+        else {
+            balance = balance+num;
+            return true;
+        }
+
+    }
+
+
+
+}
+
+class Customer{
+    String name;
+    String id;
+    String number;
+    String addr;
+
+    public String say(){
+        String info = "name:"+name+"\n"+"id:"+id+"\n"+"number:"+number+"\n"+"address:"+addr;
+        return info;
+    }
+
+
 }
--- java/src/Sort/Sort_test.java
+++ java/src/Sort/Sort_test.java
@@ -1,15 +1,45 @@
+package Sort;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
-
-public class Sort {
+public class Sort_test {
     public static void main(String args[]){
-
+        int[][] intervals = new int[][]{{1,3},{2,6},{8,10},{15,18}};
+        Solution solution = new Solution();
+        System.out.println(solution.merge(intervals));
     }
 }
 
 
-class int[][] merge(int[][] intervals) {
+class Solution{
+    //1.合并区间
+    //https://leetcode-cn.com/problems/merge-intervals/solution/he-bing-qu-jian-by-leetcode-solution/
+    public int[][] merge(int[][] intervals){
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] intervals1, int[] intervals2) {
+                return intervals1[0] - intervals2[0];
+            }
+        });
+        List<int[]> result = new ArrayList<int[]>();
+        for(int i=0;i< intervals.length;i++){
+
+            if(result.size()==0 || result.get(result.size() - 1)[1] < intervals[i][0]){
+                result.add(intervals[i]);
+            }
+            else if(result.get(result.size()-1)[1] >= intervals[i][0]){
+                result.get(result.size()-1)[1] = Math.max(result.get(result.size() - 1)[1], intervals[i][1]);
+
+            }
+
+        }
+        return result.toArray(new int[result.size()][]);
+
+    }
 
 
 }
-
 
